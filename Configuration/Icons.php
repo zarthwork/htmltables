30bd7047c89082587e075ce3a6fa2663f139371b
M. Zarth <142113713+zarthwork@users.noreply.github.com>
2023-08-17 02:41:02 +0200

    [FEATURE] New icon for content element wizard

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+   return [
+       'content-table-plus' => [
+           'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+           'source' => 'EXT:htmltables/Resources/Public/Icons/content-table-plus.svg',
+       ],
+   ];
